e2d9a66e1bf8348250fe0821503bd6996718430a
Bartłomiej Wiercigroch <landupcmr@gmail.com>
2023-10-19 20:11:49 +0200

    Init

--- index.py
+++ index.py
@@ -1,1 +1,9 @@
-print('test');
+from skimage.metrics import peak_signal_noise_ratio as psnr
+import cv2 as cv
+
+src1 = cv.imread('images/klasa1.jpg')
+src1 = cv.cvtColor(src1, cv.COLOR_BGR2GRAY)
+dst1 = cv.equalizeHist(src1)
+
+psnr_value = psnr(src1, dst1)
+print(psnr_value)
